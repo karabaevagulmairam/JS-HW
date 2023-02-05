101189d87a0db20d310eb3ee7322ff611e94a33d
karabaevagulmairam <karabaeva.gulmairam.pi9.7.20@gmail.com>
2023-02-05 22:38:41 +0600

    1/2-hw

---
+++ h_w_1/main1.js
@@ -0,0 +1,33 @@
+//1
+let a = 4
+console.log(a)
+//2
+let test = 'Hello world'
+console.log(typeof test)
+//3
+let b = 12
+    b = 5
+console.log(typeof b)
+//4
+let n = 123
+alert(n)
+//5
+let str = 'Hello world'
+    str = 45
+console.log(typeof str)
+//6
+let num = 123
+console.log(typeof String(str))
+//7
+let bool = true
+console.log(typeof +true)
+//8
+let c = null
+console.log(+null)
+let d = undefined
+console.log(+undefined)
+//9
+let t = true
+let f = false
+console.log(+f)
+console.log(+t)
---
+++ h_w_2/main2.js
@@ -0,0 +1,63 @@
+// //1
+// let num = 12
+// console.log(num)
+// //2
+// let a = 10
+//     b = 2
+//     c = 5
+// console.log(a + b + c)
+// //3
+// let d = 100
+// console.log(d * 2)
+// //4
+// let e = 67
+// console.log(67 % 7)
+//5
+// let a = 10
+// let b = 2
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a-b)
+// console.log(a+b)
+// //6
+// let c = 15
+// let d = 2
+// let result = c + d
+// console.log(result)
+//7
+// let a = 30
+// let b = 15
+// let c = a + b
+// console.log(c % 4)
+//8
+// let a = 17
+//     b = 10
+//     c = a - b
+//     d = 13
+//     result = c + d
+// console.log(result)
+//9
+// let a = '10'
+//     b = '99'
+//     result = a + b
+// console.log(result)
+//10
+let name = 'лет'
+    number = 10
+    result = name + number
+console.log(result)
+//11
+let a = 123
+console.log(String(a))
+//12
+let b = true
+console.log(+b)
+//13
+let c = '12s'
+    d = '12.2'
+console.log(+c)
+console.log(Number(d))
+//14 и 15 я уже сделали выше
+//16 Бинарные операоры применяются к двум аргументам 2+2,2-2,2/2,2*2,2%2,2**2
+//17 Унарные операторы применяются к одному аргументу -2, +2 и тд
+
